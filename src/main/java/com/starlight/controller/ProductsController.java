5d643cdd457c3b35407e353555e459190f0e3fae
Neiklot <dbenitez83@live.com>
2017-07-04 19:41:35 +0200

    server enviando datos a APP

--- src/main/java/com/starlight/controller/ProductsController.java
+++ src/main/java/com/starlight/controller/ProductsController.java
@@ -1,22 +1,36 @@
 package com.starlight.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.starlight.model.Product;
+import com.starlight.services.ProductService;
 
 @Controller
 public class ProductsController {
+	
+	@Autowired
+	private ProductService productService;
 
 	
 	@RequestMapping(value = "/product/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> getProduct(@PathVariable("id") long id) {
-		Product product=new Product();
-		product.setName("Cocacola");
+		Product product=productService.getProducts(id);
 		return new ResponseEntity<Product>(product, HttpStatus.OK);
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8100")
+	@RequestMapping(value = "/product", method = RequestMethod.GET)
+	public ResponseEntity<?> getProducts() {
+		List<Product> products=productService.getAllProducts();
+		return new ResponseEntity(products, HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/starlight/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.starlight.services;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Service;
+
+import com.starlight.model.Product;
+
+@Service
+public class ProductService {
+
+	  @PersistenceContext
+	  private EntityManager em;
+	  
+	  public Product getProducts(Long id) {
+		  Product product=em.find(Product.class,id);
+		  return product;
+	  }
+	  
+	  public List<Product> getAllProducts() {
+		 Query q=em.createNativeQuery("Select * from Product", Product.class);
+		 List<Product> products=q.getResultList();
+		 return products;
+	  }
+}
