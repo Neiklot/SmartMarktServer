a70f3544785884b26bf79b3e60726c6466a21369
Neiklot <dbenitez83@live.com>
2017-07-22 09:57:24 +0200

    crear nuevo producto

--- src/main/java/com/starlight/controller/ProductsController.java
+++ src/main/java/com/starlight/controller/ProductsController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,6 +40,13 @@ public class ProductsController {
 		List<Product> products = productService.getAllProducts();
 		return new ResponseEntity(products, HttpStatus.OK);
 	}
+	
+	@RequestMapping(value = "/product", method = RequestMethod.POST,consumes="application/json")
+	public ResponseEntity<?> createProduct(@RequestBody Product product ) {
+		Long idProduct=productService.createProduct(product);
+		return new ResponseEntity(idProduct, HttpStatus.OK);
+	}
+	
 
 	@RequestMapping(value = "/product/{idProduct}/getImage", method = RequestMethod.GET)
 	@ResponseBody
--- src/main/java/com/starlight/services/ProductService.java
+++ src/main/java/com/starlight/services/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
@@ -13,17 +14,23 @@ import com.starlight.model.Product;
 @Service
 public class ProductService {
 
-	  @PersistenceContext
-	  private EntityManager em;
-	  
-	  public Product getProducts(Long id) {
-		  Product product=em.find(Product.class,id);
-		  return product;
-	  }
-	  
-	  public List<Product> getAllProducts() {
-		 Query q=em.createNativeQuery("Select * from Product", Product.class);
-		 List<Product> products=q.getResultList();
-		 return products;
-	  }
+	@PersistenceContext
+	private EntityManager em;
+
+	public Product getProducts(Long id) {
+		Product product = em.find(Product.class, id);
+		return product;
+	}
+
+	public List<Product> getAllProducts() {
+		Query q = em.createNativeQuery("Select * from Product", Product.class);
+		List<Product> products = q.getResultList();
+		return products;
+	}
+
+	@Transactional
+	public Long createProduct(Product product) {
+		em.persist(product);
+		return product.getId();
+	}
 }
